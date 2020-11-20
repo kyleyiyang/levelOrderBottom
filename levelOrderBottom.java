6a28eb4103e9733176d57b6c14406979ce385ce1
kyleyiyang <kyleyiyang@gmail.com>
2020-11-19 22:56:06 -0500

    Create levelOrderBottom.java

---
+++ levelOrderBottom.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root==null) return ans;
+        ans.add(new ArrayList<Integer>(Arrays.asList(root.val)));
+        int level=0;
+        traversal(root,ans,level);
+        Collections.reverse(ans);
+        return ans;
+    }
+    public static void traversal(TreeNode node, List<List<Integer>> ans, int level) {
+        level++;
+        List<Integer> temp=new ArrayList<>();
+        if (node!=null) {
+            
+            if (node.left!=null) temp.add(node.left.val);
+            if (node.right!=null) temp.add(node.right.val);
+            if (temp.size()>0) {
+                if (ans.size()>level) {
+                    List<Integer> re = ans.get(level);
+                    List<Integer> newList = Stream.concat(re.stream(), temp.stream()).collect(Collectors.toList());
+                    ans.set(level, newList);
+                } else {
+                    ans.add(level,temp);
+                }
+            }
+            traversal(node.left, ans, level);
+            traversal(node.right, ans, level);
+        }
+    }
+}

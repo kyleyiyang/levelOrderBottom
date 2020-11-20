1cdd53d0a865a35a64763c72b6116c2076e24c17
kyleyiyang <kyleyiyang@gmail.com>
2020-11-19 23:02:43 -0500

    Update levelOrderBottom.java

--- levelOrderBottom.java
+++ levelOrderBottom.java
@@ -20,7 +20,7 @@ class Solution {
         ans.add(new ArrayList<Integer>(Arrays.asList(root.val)));
         int level=0;
         traversal(root,ans,level);
-        Collections.reverse(ans);
+        //Collections.reverse(ans);
         return ans;
     }
     public static void traversal(TreeNode node, List<List<Integer>> ans, int level) {
@@ -32,11 +32,11 @@ class Solution {
             if (node.right!=null) temp.add(node.right.val);
             if (temp.size()>0) {
                 if (ans.size()>level) {
-                    List<Integer> re = ans.get(level);
+                    List<Integer> re = ans.get(ans.size()-level-1);
                     List<Integer> newList = Stream.concat(re.stream(), temp.stream()).collect(Collectors.toList());
-                    ans.set(level, newList);
+                    ans.set(ans.size()-level-1, newList);
                 } else {
-                    ans.add(level,temp);
+                    ans.add(0,temp);
                 }
             }
             traversal(node.left, ans, level);
